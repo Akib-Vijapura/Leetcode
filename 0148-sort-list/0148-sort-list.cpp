86d70c8c8bc039ac7b9a0cbfa53ebac3468d57ca
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-08-23 22:31:22 +0530

    Time: 1766 ms (5%), Space: 58.2 MB (74.59%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+       
+       //This solution is use insertion sort algorithm
+
+        if (!head) {
+            return nullptr;
+        }
+
+        vector<int> arr;
+        ListNode* temp = head;
+
+        while (temp) {
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        for (int i = 1; i < arr.size(); i++) {
+            int curr = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > curr) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = curr;
+        }
+
+        temp = head;
+
+        for (auto num : arr) {
+            temp->val = num;
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
