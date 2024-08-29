85b658f8ac5d41e45fe367e8abb7974cb1432f6b
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-08-29 17:24:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (61.43%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
 
-        if(nums.size() <= 1){
-            return nums[0];
-        }
+
 
         int rob1 = 0;
         int rob2 = 0;
-        int maxAmount1 = 0;
+        int maxAmount1 = nums[0];
         int maxAmount2 = 0;
 
         for (int i = 0; i < nums.size() - 1; i++) {
