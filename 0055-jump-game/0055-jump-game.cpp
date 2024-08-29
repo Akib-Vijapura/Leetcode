45312ff59c781edfd0ddadd2260db7402760bf9b
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-08-29 18:13:52 +0530

    Time: 37 ms (86.36%), Space: 51.1 MB (38.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int jump = 0;
+
+        for(int i = 0  ; i < n ; i++)
+        {
+            if(jump < i){
+                return false;
+            }
+
+            jump = max(jump , i + nums[i]);
+
+            if(jump >= n-1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
