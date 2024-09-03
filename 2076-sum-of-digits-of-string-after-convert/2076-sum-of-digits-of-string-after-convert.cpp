94dcace07bf19c1b1e5085d5c99ed814553bf087
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-09-03 10:20:43 +0530

    Time: 3 ms (51.28%), Space: 10.1 MB (38.94%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string number;
+
+        for(auto c : s){
+            number += to_string(c - 'a' + 1);
+        }
+
+        while(k > 0){
+            int sum = 0;
+
+            for(auto c :  number){
+                sum += c - '0';
+            }
+
+            number = to_string(sum);
+            k--;
+        }
+
+        return stoi(number);
+    }
+};
