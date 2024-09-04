5c334b83194e54815590f2b4f45539c684401163
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-09-04 10:04:00 +0530

    Time: 35 ms (31.66%), Space: 12.3 MB (48.03%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,20 +1,26 @@
 class Solution {
 private:
-   bool solve(int i , int j , string& s){
+    int dp[1001][1001];
+    bool solve(int i , int j , string& s){
         if(i >= j){
             return true;
         }
 
-        if(s[i] == s[j]){
-            return solve(i+1 , j-1 , s);
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        return false;
+        if(s[i] == s[j]){
+            return dp[i][j] =  solve(i+1 , j-1 , s);
+        }
+
+        return dp[i][j] =  false;
    }
 public:
     int countSubstrings(string s) {
         int count = 0;
         int n = s.size();
+        memset(dp , -1 , sizeof(dp));
 
         for(int i = 0 ; i < n ; i++){
             for(int j = i ; j < n ; j++){
