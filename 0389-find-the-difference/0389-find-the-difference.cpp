4b9553053782a70f3d8b0b578467c8cab6e2252b
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-09-04 11:24:04 +0530

    Time: 2 ms (69.33%), Space: 8.1 MB (79.67%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        
-        unordered_map<char , int> mp;
+        int n = s.size();
+        int m = t.size();
 
-        for(auto c : t){
-            mp[c]++;
+        int ASCIIs = 0;
+        int ASCIIt = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            ASCIIs += s[i] - 'a';
+            ASCIIt += t[i] - 'a';
         }
 
-        
+        ASCIIt += t[m-1] - 'a';
 
-        for(auto c : s){
-            mp[c]--;
-        }
-
-        char ans;
-        for(auto it : mp){
-            if(it.second > 0){
-                ans = it.first;
-            }
-        }
-        return ans;
+        return ASCIIt - ASCIIs + 'a';
     }
 };
