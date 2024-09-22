fad0989e8203e55526f3d5bbcf297ec59bd453bc
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-09-22 10:21:32 +0530

    Time: 3 ms (73.8%), Space: 8.4 MB (98.91%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i = 1;
+        /*int i = 1;
         int count = 0;
 
         while(true){
@@ -16,6 +16,22 @@ public:
             i++;
         }
 
-        return count;
+        return count;*/
+
+        long long int low = 0;
+        long long int high = n;
+
+        while (low <= high) {   
+            long long int mid = low + (high - low) / 2;
+
+            if ((mid * (mid + 1)) / 2 == n)
+                return mid;
+            else if ((mid * (mid + 1)) / 2 > n)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return high;
     }
 };
