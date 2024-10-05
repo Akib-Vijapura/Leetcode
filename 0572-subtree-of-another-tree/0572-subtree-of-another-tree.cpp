1c4f6cef7341af4686bc23a5d0659eaded844dee
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-10-05 20:55:38 +0530

    Time: 23 ms (21.23%), Space: 28.1 MB (56.93%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+private:
+    bool isSameTree(TreeNode* s, TreeNode* t) {
+        if (s ==NULL && t==NULL) return true; // Both trees are empty
+        if (s==NULL || t==NULL) return false; // One tree is empty, and the other is not
+        if (s->val != t->val) return false; // Values do not match
+        // Recursively check left and right subtrees
+        return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == NULL) return false; // If root is null, subRoot cannot be a subtree
+        if(isSameTree(root, subRoot)) return true; // Check if trees are identical
+        // Recursively check left and right subtrees
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
