6c73c12ce948cbb3f03f2d0bb65ea9a2056e67bb
Akib Vijapura <72021210+Akib-Vijapura@users.noreply.github.com>
2024-10-05 21:01:08 +0530

    Time: 15 ms (76.6%), Space: 28.1 MB (16.12%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -1,18 +1,42 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    bool isSameTree(TreeNode* s, TreeNode* t) {
-        if (s ==NULL && t==NULL) return true; // Both trees are empty
-        if (s==NULL || t==NULL) return false; // One tree is empty, and the other is not
-        if (s->val != t->val) return false; // Values do not match
-        // Recursively check left and right subtrees
-        return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
-    }
 public:
+
+    bool solve(TreeNode* root, TreeNode* subRoot){
+        if(root == nullptr && subRoot == nullptr){
+            return true;
+        }
+
+        if(root == nullptr || subRoot == nullptr){
+            return false;
+        }
+
+        if(root->val != subRoot->val){
+            return false;
+        }
+
+        return solve(root->left , subRoot->left) && solve(root->right , subRoot->right);
+    }
+
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL) return false; // If root is null, subRoot cannot be a subtree
-        if(isSameTree(root, subRoot)) return true; // Check if trees are identical
-        // Recursively check left and right subtrees
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        if(root == nullptr){
+            return false;
+        }
+
+        if(solve(root , subRoot)){
+            return true;
+        }
+
+        return isSubtree(root->left , subRoot) || isSubtree(root->right , subRoot);
     }
 };
